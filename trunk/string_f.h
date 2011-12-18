bbe666dcec2b00461abcbb8470e58e8e2e63c157
xavier.andrade.valencia@gmail.com <xavier.andrade.valencia@gmail.com@4c667ac9-2d09-66dd-ce78-784e66efd939>
2011-12-18 00:48:54 +0000

    * Added LGPL notice to all files. * Changed "This program" to FortranCL in the LGPL notice.

--- trunk/string_f.h
+++ trunk/string_f.h
@@ -1,12 +1,12 @@
 /*
  Copyright (C) 2003 M. Marques, A. Castro, A. Rubio, G. Bertsch
 
- This program is free software; you can redistribute it and/or modify
+ FortranCL is free software; you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation; either version 2, or (at your option)
  any later version.
 
- This program is distributed in the hope that it will be useful,
+ FortranCL is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Lesser General Public License for more details.
